9bbfd062356a235394b540d03d1a1c08424996b9
Jeetgaming <64637476+Jeetgaming@users.noreply.github.com>
2021-04-27 19:12:40 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,23 @@
+class PlinkoClass{
+    constructor(x,y){
+
+        var options = {
+            isStatic: true
+        }
+
+        
+        this.body = Bodies.circle(x,y, 10, options);
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        
+        push();
+        translate(pos.x,pos.y);
+        noStroke();
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,10,10);
+        pop();
+    }
+};
